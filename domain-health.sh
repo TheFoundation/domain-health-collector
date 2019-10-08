ee39fbe89e101fad5884361845c1b57504e6f6ff
Benjamin Freitag <benchonaut@yandex.com>
2019-10-08 21:22:47 +0200

    2019-10-08-19.22

--- domain-health.sh
+++ domain-health.sh
@@ -58,7 +58,7 @@ cat /tmp/vhostconf.docker.domainlist  |while read a ;do ( target="";type=${a/%@*
 if [ "$type" == "H" ];then  url=$(echo $a|cut -d" " -f1|cut -d@ -f3|cut -d"," -f1); http_stat=$(curl -sw '%{http_code}' https://$url -o /dev/null 2>&1);fi
 if [ "$type" == "R" ];then  url=$(echo $a|cut -d" " -f1|cut -d@ -f3|cut -d"," -f1);http_stat=$(curl -sw '%{http_code}' $url -o /dev/null 2>&1); target=$(curl -Ls -w %{url_effective} -o /dev/null $url) ; fi; echo $http_stat"@"$a"@"$target ) & done
 test -d /tmp/.domain-health-lists/.git || (rm -rf /tmp/.domain-health-lists/.git; git clone $CLIENT_GIT_REPO /tmp/.domain-health-lists)
-cd /tmp/.domain-health-lists/.git
+cd /tmp/.domain-health-lists/
 
 wait
  echo -n ; } ;
