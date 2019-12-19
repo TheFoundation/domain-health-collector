618493b48cc09ae0f2271189691574a9b8b36a47
root <root@system>
2019-12-19 17:04:22 +0100

    2019-12-19-16.04

--- domain-health.sh
+++ domain-health.sh
@@ -107,16 +107,15 @@ _host_extract() {
 		
 		esac
 		test -d /tmp/domainlist-$(hostname -f) || mkdir /tmp/domainlist-$(hostname -f)
-		test -d /tmp/domainlist-$(hostname -f) && ( 
-		cat /tmp/vhostconf.domainlist > /tmp/domainlist-$(hostname -f)/domainlist
-		test -d /tmp/domainlist-$(hostname -f)/.git && rm -rf /tmp/domainlist-$(hostname -f)/.git 
-		cd /tmp/domainlist-$(hostname -f); 
-		git init
-		git remote add origin $HOST_GIT_REPO
-		git add domainlist
-		git commit -am $(hostname -f)"domain list "$(date -u +%Y-%m-%d-%H.%M)
-		git config user.name domainlist@$(hostname -f)
-		git push -f origin master
+#		test -d /tmp/domainlist-$(hostname -f) && ( 
+#		test -d /tmp/domainlist-$(hostname -f)/.git && rm -rf /tmp/domainlist-$(hostname -f)/.git 
+#		cd /tmp/domainlist-$(hostname -f); 
+#		git init
+#		git remote add origin $HOST_GIT_REPO
+
+		test -d /tmp/domainlist-$(hostname -f)/.git && rm -rf /tmp/domainlist-$(hostname -f)/
+		git clone $HOST_GIT_REPO /tmp/domainlist-$(hostname -f)/ && cat /tmp/vhostconf.domainlist > /tmp/domainlist-$(hostname -f)/domainlist
+		cd /tmp/domainlist-$(hostname -f) &&	git add domainlist && 	git commit -am $(hostname -f)"domain list "$(date -u +%Y-%m-%d-%H.%M) && git config user.name domainlist@$(hostname -f) && git push -f origin master
 		)
 		echo -n ; } ;
 
